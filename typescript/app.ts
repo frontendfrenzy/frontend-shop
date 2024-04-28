a6262786e685d12cdcbe81546f75facfed625bd4
faleel <168091537+frontendfrenzy@users.noreply.github.com>
2024-04-28 10:25:40 +0530

    Update app.ts

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,30 @@
-console.log("hello world");
+//Typesript tutorial
+let message:string = "hello,world";
+console.log(message);
+
+//Number 
+let x:number = 100,
+y:number = 150;
+console.log(x+y);
+
+//String
+let name:string = `john`;
+let job:string = `front developer`;
+let profile:string = `${name} was a ${job}`;
+console.log(profile);
+
+//Object
+let employee:{
+  firstname:string;
+  lastname:string;
+  age:number;
+  company:string;
+}
+employee = {
+  firstname:"faleel",
+  lastname:"ahmed",
+  age:18,
+  company:"microsoft"
+}
+console.log(employee.firstname);
+
